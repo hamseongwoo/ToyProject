37c748cbc75182ca26591fce3f3f847fe00acd1d
함성우 <a01098679934@gmail.com>
2025-02-21 02:25:12 +0900

    세번째 커밋

--- src/page/Login.js
+++ src/page/Login.js
@@ -4,12 +4,20 @@ function Login() {
   return (
     <>
       <div className="login-container">
+        <h1>로그인</h1>
         <label className="login-label">이메일</label>
         <input
           type="text"
           className="login-input"
           placeholder="이메일을 입력해주세요"
         />
+        <label className="login-label">비밀번호</label>
+        <input
+          type="password"
+          className="login-input"
+          placeholder="비밀번호를 입력해주세요"
+        />
+        <button type="submit" className="login-button">로그인</button>
       </div>
     </>
   );
