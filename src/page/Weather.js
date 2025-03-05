a8f54455ed34747ff293cdb41d2413c9fe57f04a
함성우 <a01098679934@gmail.com>
2025-03-05 09:07:47 +0900

    search 추가

--- src/page/Weather.js
+++ src/page/Weather.js
@@ -5,7 +5,10 @@ function Weather() {
     <>
       <div className="screen">
         <div className="container">
-            <div className="search"></div>
+            <div className="search">
+                <input type="text" className="city" placeholder="도시를 입력하세요" />
+                <button onClick="search()">검색</button>
+            </div>
         </div>
       </div>
     </>
