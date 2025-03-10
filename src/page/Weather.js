be614689a233697c297bb59783542a6bdbdcd408
함성우 <a01098679934@gmail.com>
2025-03-10 16:56:19 +0900

    수정2

--- src/page/Weather.js
+++ src/page/Weather.js
@@ -20,19 +20,34 @@ function Weather() {
   }
 
   return (
-      <div className="screen">
-        <div className="container">
-          <div className="search">
-            <input
-              type="text"
-              value={city}
-              onChange={(e) => setCity(e.target.value)}
-              placeholder="도시를 입력하세요"
-            />
-            <button onClick={() => getWeather(city)}>검색</button>
-          </div>
+    <div className="screen">
+      <div className="container">
+        <div className="search">
+          <input
+            type="text"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+            placeholder="도시를 입력하세요"
+          />
+          <button onClick={() => getWeather(city)}>검색</button>
         </div>
+
+        {weather && (
+          <>
+            <div className="area">검색 지역: {weather.name}</div>
+            <div className="description">전체적인 날씨: {weather.weather[0].description}</div>
+            <div className="clouds">흐림 정도: {weather.clouds.all}%</div>
+            <div className="wind">풍속: {weather.wind.speed}m/s</div>
+            <div className="temp">현재 온도: {weather.main.temp}°C</div>
+            <div className="feels_like">체감 온도: {weather.main.feels_like}°C</div>
+            <div className="temp_min">최저 기온: {weather.main.temp_min}°C</div>
+            <div className="temp_max">최고 기온: {weather.main.temp_max}°C</div>
+            <div className="pressure">기압: {weather.main.pressure} hPa</div>
+            <div className="humidity">습도: {weather.main.humidity}%</div>
+          </>
+        )}
       </div>
+    </div>
   );
 }
 
