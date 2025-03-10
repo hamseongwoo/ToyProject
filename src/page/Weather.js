608af7862d9f9193e7b189249f3b0e4b6cb773c2
함성우 <a01098679934@gmail.com>
2025-03-10 16:55:48 +0900

    수정

--- src/page/Weather.js
+++ src/page/Weather.js
@@ -20,30 +20,19 @@ function Weather() {
   }
 
   return (
-    <>
       <div className="screen">
         <div className="container">
           <div className="search">
             <input
               type="text"
-              className="city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
               placeholder="도시를 입력하세요"
             />
-            <button className="button">검색</button>
+            <button onClick={() => getWeather(city)}>검색</button>
           </div>
-          <div className="area"></div>
-          <div className="description"></div>
-          <div className="clouds"></div>
-          <div className="wind"></div>
-          <div className="feels_like"></div>
-          <div className="humidity"></div>
-          <div className="pressure"></div>
-          <div className="temp"></div>
-          <div className="temp_max"></div>
-          <div className="temp_min"></div>
         </div>
       </div>
-    </>
   );
 }
 
