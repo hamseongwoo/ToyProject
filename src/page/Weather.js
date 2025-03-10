ea531eef711dd4f8173e435a3e09709670f0be89
함성우 <a01098679934@gmail.com>
2025-03-10 16:07:54 +0900

    usestate사용

--- src/page/Weather.js
+++ src/page/Weather.js
@@ -1,45 +1,24 @@
 import "./Weather.css";
+import { useState, useEffect } from "react";
 
 const API_KEY = "dee6c11b0dc85f0c38be60e7ef8b696d";
 const BASE_URL = "http://api.openweathermap.org/data/2.5/weather";
 
-const temp = document.getElementById("temp");
-const feels_like = document.getElementById("feels_like");
-const temp_min = document.getElementById("temp_min");
-const temp_max = document.getElementById("temp_max");
-const pressure = document.getElementById("pressure");
-const humidity = document.getElementById("humidity");
-
-const clouds = document.querySelector(".clouds");
-const wind = document.querySelector(".wind");
-const description = document.querySelector(".description");
-const area = document.querySelector(".area");
-
-async function getWeather() {
-  try {
-    const url = `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric&lang=kr`;
-    const response = await fetch(url);
-    const data = await response.json();
-
-    area.innerText = `검색 지역: ${data.name}`;
-    description.innerText = `전체적인 날씨: ${data.weather[0].description}`;
-    clouds.innerText = `흐림 정도: ${data.clouds.all}%`;
-    wind.innerText = `풍속: ${data.wind.speed}m/s`;
-
-    temp.innerText = `현재 온도: ${data.main.temp}°C`;
-    feels_like.innerText = `체감 온도: ${data.main.feels_like}°C`;
-    temp_min.innerText = `최저 기온: ${data.main.temp_min}°C`;
-    temp_max.innerText = `최고 기온: ${data.main.temp_max}°C`;
-    pressure.innerText = `기압: ${data.main.pressure} hPa`;
-    humidity.innerText = `습도: ${data.main.humidity}%`;
-
-    
-  } catch (error) {
-    console.error("오류 발생", error);
-  }
-}
-
 function Weather() {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("seoul");
+
+  async function getWeather(city) {
+    try {
+      const url = `${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric&lang=kr`;
+      const response = await fetch(url);
+      const data = await response.json();
+      setWeather(data);
+    } catch (error) {
+      console.error("오류 발생", error);
+    }
+  }
+
   return (
     <>
       <div className="screen">
@@ -56,12 +35,12 @@ function Weather() {
           <div className="description"></div>
           <div className="clouds"></div>
           <div className="wind"></div>
-          <div weatherData className="feels_like"></div>
-          <div weatherData className="humidity"></div>
-          <div weatherData className="pressure"></div>
-          <div weatherData className="temp"></div>
-          <div weatherData className="temp_max"></div>
-          <div weatherData className="temp_min"></div>
+          <div className="feels_like"></div>
+          <div className="humidity"></div>
+          <div className="pressure"></div>
+          <div className="temp"></div>
+          <div className="temp_max"></div>
+          <div className="temp_min"></div>
         </div>
       </div>
     </>
